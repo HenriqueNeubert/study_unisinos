3bcd709180763ec9e0e9327ad05c75159bcffbc4
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-03-22 11:45:17 -0300

    feat: content about for, while, if and metodos added in rascunhos.py

--- rascunhos.py
+++ rascunhos.py
@@ -30,11 +30,93 @@
 #media = (valor1 + valor2 + valor3) / 3
 #print("Media dos valores:", media)
 
+########################################################################
+#IMPRESSÃO BÁSICA
+print("Hello, world!")
+#SOLICITANDO DADOS
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+peso = float(input("Digite seu peso:"))
+#VERIFICA TIPO DE DADOS
+type(nome)
+#QUEBRA DE LINHA
+print("Hello\nWorld")
+#FORMATAÇÃO
+#UTILIZANDO (.format)
+#format= outra maneira de trabalhar 
+#com imprerssão de dados em variaveis
+print("Ola {} sua idade é {} anos.".format(nome,idade))
+print("Inteiro: {:d}".format(12354))
+#saída: 12354
+print("Zero à esquerda: {:04d}".format(25))
+#saída: 0025
+print("Casas decimais: {:.2f}".format(2.545445))
+#saída: 2.54
 
+#IF - ELSE - ELIF
+number = 5
+if number > 0:
+	print("POSITIVO")
+elif number == 0:
+	print("0")
+else:
+  print("NEGATIVO")
 
+#WHILE
+i = 0
+while i < 100:
+	print("valor de i:", i)
+	i += 1
+	if i == 32:
+		break 
+		#brak=para 
 
+op = int(input("Digite 3 para sair:"))
+while op != 3:
+	print("Você digitou", op, "\nVocê não saiu.")
+	op = int(input("Digite 3 para sair:"))
+	
+#FOR
+semana = [
+"SEGUNDA",
+"TERÇA",
+"QUARTA",
+"QUINTA",
+"SEXTA",
+"SÁBADO",
+"DOMINGO"
+]
 
+for dia in semana:
+	print(dia)
 
+#ex2
+for num in range(1,6):
+	print(num)
+	
+#CRIAR LISTA
+#cria uma lista vazia
+lista = []
+#para adicionar um item 
+lista.append(1)
+lista.append(2)
+lista.append(3)
+lista.append(4)
+lista.append(5)
+for item in lista:
+	print(item)
+	
+#METODOS
+#metodo
+def meuMetodo(cor):
+	print(cor)#imprime a string "AZUL"
+#chama o metodo
+#"AZUL é o parâmetro"
+meuMetodo("AZUL")
+
+def soma(a,b):
+	return a+b
+soma(2,3) 
 
 
 
