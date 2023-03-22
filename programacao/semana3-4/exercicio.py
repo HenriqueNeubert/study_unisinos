6adb1ebb2392a6eb58d070af2b7d424d3a0357ce
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-03-22 20:58:36 -0300

    feat: complete exercise 1,2 and 3 in semana3-4/exercicio.py

--- programacao/semana3-4/exercicio.py
+++ programacao/semana3-4/exercicio.py
@@ -1,1 +1,102 @@
 #EXERCICIO_(1)
+  #Crie um programa que recebe um inteiro pelo teclado e imprime se 
+  # ele é par ou ímpar.  
+  
+# number = int(input("Digite um número:"))
+# if number % 2 == 0 :
+#   print("PAR")
+# else:
+#   print("IMPAR")
+     
+#EXERCICIO_(2)
+  # Crie um programa que recebe dois valores inteiros A e B pelo teclado e imprime 
+  # o valor de A dividido por B. Entretanto, se o valo de B for 0, imprima uma 
+  # mensagem de erro e não faça a divisão.
+
+# a = int(input("Digite um número (A):"))
+# b = int(input("Digite um número (B):"))
+
+# if b != 0:
+#   resultado = a / b
+#   print("Resultado:", resultado)
+# else:
+#   print("ERRO:", b)
+
+#EXERCICIO_(3)
+  # Crie um programa que recebe um valor inteiro referente a um determinado ano. 
+  # Imprima na tela se o ano informado é bissexto ou não (para simplificar, você 
+  # pode utilizar apenas a informação de o ano é divisível por 4 ou não).
+
+# ano = int(input("Digite um ano:")) 
+# if ano / 4 == 0:
+#   print("O ano é bissexto:", ano)
+# else:
+#   print("O ano não é bissexto:", ano)
+
+#EXERCICIO_(4)
+  # Crie um programa que recebe a nota do Grau A e a nota do Grau B pelo teclado e 
+  # imprime na tela se será necessário ou não realizar o Grau C (considere o sistema 
+  # de avaliação da Unisinos, sendo o GA valendo 30% e o GB valendo 70%). Caso algum
+  # valor informado seja negativo, informe uma mensagem de erro e não realize o 
+  # cálculo.
+
+
+ 
+#EXERCICIO_(5)
+  # Crie um programa que solicita que o usuário digite uma letra e imprime na tela
+  # se a letra é uma vogal ou uma consoante.
+
+ 
+#EXERCICIO_(6)
+  # O que é um parâmetro de entrada de um método?
+
+ 
+#EXERCICIO_(7)
+  # O que é o retorno de um método?
+
+ 
+#EXERCICIO_(8)
+  # Utilizando while, crie um programa que imprime os números de 0 a 1000. 
+
+ 
+#EXERCICIO_(9)
+  # Utilizando while, crie um programa que imprime os números pares de 0 a 2000.
+
+ 
+#EXERCICIO_(10)
+  # Utilizando while, crie um programa que imprime os números de 0 a 1000 em 
+  # ordem decrescente (ou seja, de 1000 a 0).
+
+ 
+#EXERCICIO_(11)
+  # Utilizando while, crie um programa que solicita para o usuário que ele digite 10
+  #  valores inteiros. Ao final, imprima a soma de todos os valores digitados. 
+
+ 
+#EXERCICIO_(12)
+  # Comparando os comandos de repetição for e while, em quais ocasiões é mais comum 
+  # a utilização de um ou de outro?
+
+  # O comando while geralmente é mais utilizado quando não sabemos a quantidade de 
+  # repetições que nosso código fará. Por exemplo, “enquanto o usuário não acertar 
+  # o usuário e a senha, peça novamente”. Já o comando for é mais utilizado quando 
+  # sabemos o número de iterações a realizar. Por exemplo, “o usuário possui 5 
+  # tentativas para acertar o usuário e a senha”.
+
+ 
+#EXERCICIO_(13)
+  # Utilizando for, crie um programa imprime na tela os valores de 1 
+  # a 100 (incluindo o 1 e o 100).
+
+ 
+#EXERCICIO_(14)
+  # Utilizando for,crie um programa que imprime a tabuada de um número inteiro 
+  # digitado pelo usuário.
+
+ 
+#EXERCICIO_(15)
+  # Crie um programa que permita que o usuário crie sua lista de compras. 
+  # Primeiramente, solicite que ele informe quantos produtos serão adicionados
+  # na lista. Depois disto, peça para que o usuário digite os produtos que ele 
+  # vai comprar, e armazene em uma lista. Ao final, imprima a lista de compras 
+  # do usuário.
