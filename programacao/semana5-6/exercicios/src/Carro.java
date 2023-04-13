16001582046dffcf6306a055149be3510cd5aced
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-04-13 08:51:37 -0300

    feat:  Studying Objetos, classes and metodos

---
+++ programacao/semana5-6/exercicios/src/Carro.java
@@ -0,0 +1,26 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!!!");
+//     }
+// }
+
+public class Carro {
+    //Atributos
+    String nome;
+    String marca;
+    int ano;
+    int vel;
+
+    //métodos
+    void acelerar(int acelerar){
+        vel += acelerar;
+    }
+
+    void freiar(int reduzir){
+        vel -= reduzir;
+    }
+
+    void buzinar(){
+        System.out.println("Biiiih!!!");
+    }
+}
---
+++ programacao/semana5-6/exercicios/src/CarroTestat.java
@@ -0,0 +1,16 @@
+public class CarroTestat {
+  public static void main(String[] args){
+    Carro c1 = new Carro();
+
+    c1.nome = "Celta";
+    c1.marca = "Chevrolet";
+    c1.ano = 2015;
+    c1.vel = 60;
+
+    c1.acelerar(10);
+    System.out.println("Velocidade: " + c1.vel   + "km/h");
+    c1.freiar(30);
+    System.out.println("Velocidade: " + c1.vel   + "km/h");
+
+  }
+}
