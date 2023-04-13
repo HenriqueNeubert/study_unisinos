1cf5b0f690e1573ea288214bd4238bcb384fb2d4
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-04-13 09:38:12 -0300

    feat: working with Get and Set in java

---
+++ programacao/semana5-6/exercicios/src/Aluno.java
@@ -0,0 +1,28 @@
+public class Aluno {
+  //private = modificador de acesso
+  private String nome;
+  private int idade;
+
+  //void = tipo de retorno (vazio)
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+  //String = tipo de retorno (texto)
+  public String getNome() {
+    return nome;    
+  }
+
+  public void setIdade(int idade){
+    if(idade >= 0 && idade <= 130){
+      this.idade = idade;
+      System.out.println("Idade Cadastrada");
+    }
+    else{
+      System.out.println("Idade inválida");
+    }
+  }
+
+  public int getIdade(){
+    return idade;
+  }
+}
---
+++ programacao/semana5-6/exercicios/src/AlunoTestar.java
@@ -0,0 +1,16 @@
+public class AlunoTestar {
+  public static void main(String [] args) {
+    //constructor
+    Aluno aluno1 = new Aluno();
+
+    // aluno1.nome = "Henrique";
+    //set = alterar, renomear
+    aluno1.setNome("Henrique");
+    //get = pegar
+    System.out.println(aluno1.getNome());
+
+    // aluno1.idade = 500;
+    aluno1.setIdade(50);
+    System.out.println(aluno1.getIdade());
+  }  
+}
