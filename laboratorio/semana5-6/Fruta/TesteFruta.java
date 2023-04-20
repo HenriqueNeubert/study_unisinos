1742d3ac06c7babb2eef30b3dce32983c1d379de
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-04-19 22:23:32 -0300

    feat: added contetn in class Fruta

--- laboratorio/semana5-6/Fruta/TesteFruta.java
+++ laboratorio/semana5-6/Fruta/TesteFruta.java
@@ -1,22 +1,23 @@
 public class TesteFruta{
     public static void main(String[] args){
-        Fruta item;
-        item = new Fruta("Banana", 2.50);
-        String nome = item.getNome();
-        System.out.println(nome);
+        Fruta item1;
+        Fruta item2;
+        item1 = new Fruta("Banana", 2.50);
+        item2 = new Fruta("Abacaxi", 6.00);
         
-        double preco = item.getPreco();
-        System.out.println(preco);
+        System.out.println(item1.getNome());
+        System.out.println(item2.getNome());
         
-        item.setNome("Banana Caturra");
-        item.setPreco(3.00);
+        double preco = item1.getPreco();
+        System.out.println(item1.getPreco());
         
-        preco = item.getPreco();
-        nome = item.getNome(); 
-        System.out.println("Novo nome: " + nome + " e Novo preço: " + preco);
+        item1.setNome("Banana Caturra");
+        item1.setPreco(3.00);
+        
+        System.out.println("Novo nome: " + item1.getNome() + " e Novo preço: " + item1.getPreco());
     
         double imposto = 0.5;
-        double precoFinal = item.calculaPreco(imposto);   
+        double precoFinal = item1.calculaPreco(imposto);   
         System.out.println("Preço Final: " + precoFinal);
     }
 }
