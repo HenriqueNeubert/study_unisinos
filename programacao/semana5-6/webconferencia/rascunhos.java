78ee4560382be10f8294f942ba1383920f9c4e5d
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-04-24 23:49:31 -0300

    feat: add class Teclado.java

--- programacao/semana5-6/webconferencia/rascunhos.java
+++ programacao/semana5-6/webconferencia/rascunhos.java
@@ -1,7 +1,23 @@
 package webconferencia;
-// %n = linha em branco
+
+//////////////////////////////////
+//COMANDOS DE SAIDA
+//printf %n = linha em branco
+//printf %s = tipo string
+//printf %S = tipo string em maiusculo
+//printf %f = tipo float
+//printf %.2f = tipo float 0.00
+//printf %.3f = tipo float 0.000
+//printf %06.2f = tipo float 000.00 (qt de carac incluindo a ,)
+// data (%02d/%02d/%d%n)utilizado para garantir
+  //que os valores fiquem alinhados e padronizados
+  // d = decimal
+
+//////////////////////////////////
+//COMANDOS DE ENTRADA
+
 public class rascunhos {
   public static void main(String[] args){
     System.out.printf("hello %n world!");
-}
+  }
 }
